e1295c083f97726108a258c78895f5b3deff8b39
payaldhabarde <dhabardepayal@gmail.com>
2023-11-28 14:14:22 +0530

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Home1 from './Home1'
+import { Link } from 'react-router-dom'
 
 const Home4 = () => {
   return (
     <div>
     Home  
+    <Link to={"/payal"}>Home 1 par jaao</Link>
     </div>
   )
 }
